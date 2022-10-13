cec826d5a912d8f792cbf46b7d61b1b963e88fe8
Thomas Gehrmann <s6thgehr@gmail.com>
2022-10-13 09:26:04 +0200

    Implement randomness using chainlink VRF

---
+++ hardhat.config.ts
@@ -0,0 +1,15 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-gas-reporter";
+import "dotenv/config";
+import "solidity-coverage";
+import "hardhat-deploy";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+};
+
+export default config;
